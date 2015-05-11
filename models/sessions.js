5d4f95c40ba82bbd4e5c28bd51e4ae09392a4a88
Tuan Pham <tt.pham1991+git@gmail.com>
2015-05-11 09:47:36 -0700

    Handled errors and failed queries resulting in null documents

--- models/sessions.js
+++ models/sessions.js
@@ -2,80 +2,113 @@ var Db        = require('../config/database');
 var mongoose  = require('mongoose');
 var crypto    = require('crypto')
 
+// Generates a random string to use as a session token
 function generateToken() {
 	return crypto.randomBytes(256).toString('base64');
 }
 
+// Create the schema for a session
 var SessionSchema = mongoose.Schema({
+
+	// Defaults to a string of 256 characters
     token	  : { type : String, default : generateToken },
+
+    // Defaults to the current time and expires in 7 days
     createdAt : { type : Date, default : Date.now, expires : '7d' },
+
+    // Holds the ObjectId of the user logged into this session
     userId    : { type : mongoose.Schema.Types.ObjectId, required : true}
 });
 
+// Creates a collection named sessions in MongoDB
 var SessionMongoModel = Db.model('sessions', SessionSchema);
 
+// Takes a userId as an argument and returns the token string to the callback
 function findSessionByUserId(userId, callback) {
 	SessionMongoModel.findOne({ userId : userId }, function(err, session) {
-		// TODO: Handle this error
-
-		if (session) {
-			callback(null, session.token);
+		if (err) {
+			// TODO: Error message?
+			callback(err, null);
+			return;
 		}
+		if (session)
+			callback(null, session.token);
+		else
+			callback({
+				code	: 400,
+				message : 'The session does not exist.'
+			}, null);
 	});
 }
 
+// Takes a token string as an argument and returns the userId to the callback
 function findUserByToken(clientToken, callback) {
 	SessionMongoModel.findOne({ token : clientToken }, function(err, session) {
-		// TODO: Handle this error
 		if (err) {
+			// TODO: Error message?
 			callback(err, null);
 			return;
 		}
 		if (session)
 			callback(null, session.userId);
+		else
+			callback({
+				code	: 400,
+				message : 'The session does not exists.'
+			}, null);
 	});
 }
 
+// Creates a new session and attach user ObjectId to it
 function createSession(userId, callback) {
 	var newSession = new SessionMongoModel({ userId : userId });
 
 	newSession.save(function (err, session) {
-		// TODO: Handle this error
-
+		if (err) {
+			// TODO: Error message?
+			callback(err, null);
+			return;
+		}
 		if (session)
 			callback(null, session);
+		else 
+			callback({
+				code	: 400,
+				message : 'Session creation failed.'
+			}, null);
 	});
 }
 
+// Takes a token string and deletes the session with that string from MongoDB.
 function deleteSession(clientToken, callback) {
-	SessionMongoModel.findOneAndRemove({ token : clientToken }, function(err, doc) {
-		// TODO: Handle this error
+	SessionMongoModel.findOneAndRemove({ token : clientToken }, function(err, session) {
 		if (err) {
+			// TODO: Error message?
 			callback(err);
 			return;
 		}
-		if (!doc) {
+		if (session)
 			callback({
 				code	: 400,
-				message : 'Session no longer exists.'
+				message : 'Session deletion failed.'
 			});
-			return;
-		}
-		callback(null);
+		else 
+			callback(null);
 	});
 }
 
+// Takes a token string and update the createdAt field to Date.now()
 function refreshSession(clientToken, callback) {
 	SessionMongoModel.findOneAndUpdate({ token : clientToken },
 									   {$set : { createdAt : Date.now }},
-									   function(err) {
-		// TODO: Handle this error
+									   function(err, session) {
 		if (err) {
+			// TODO: Error message?
 			callback(err);
 			return;
 		}
 		callback(null);
-	})
+	});
 }
 
 
